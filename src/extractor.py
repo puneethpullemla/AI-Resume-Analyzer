71b63aec15ecbbedec8d6e5865cfbbd5cec9448c
puneeth pullemla <puneethpullemla@gmail.com>
2026-02-24 23:39:43 +0530

    changed fitz to pdfplumber

--- src/extractor.py
+++ src/extractor.py
@@ -1,11 +1,14 @@
-import fitz
+import pdfplumber
 
 def extract_text_from_pdf(pdf_path):
-    text  = ""
-    
-    doc = fitz.open(pdf_path)
-    
-    for page in  doc:
-        text += page.get_text()
-        
+    text = ""
+
+    try:
+        with pdfplumber.open(pdf_path) as pdf:
+            for page in pdf.pages:
+                text += page.extract_text() or ""
+    except Exception as e:
+        print("Error reading PDF:", e)
+        return ""
+
     return text
